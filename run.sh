63012daedff17a827b67a4f1c95928c6ee566992
Herman2277 <132544316+Herman2277@users.noreply.github.com>
2024-05-22 16:29:01 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-c6b1af69-9deb-4679-8d03-f5bd398ab384/)
 
 
 echo "$curl_output" >> Update.tsx
